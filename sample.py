8e8012bbf5c0a4525c90fbbdaf65ef596d3c3018
Partha <parthadash779@gmail.com>
2026-01-19 20:49:41 +0530

    Refactor: sample.py

---
+++ sample.py
@@ -0,0 +1,31 @@
+from dataclasses import dataclass
+from typing import Iterable, Union, Tuple
+
+@dataclass(frozen=True)
+class Number:
+    value: float
+
+    @classmethod
+    def parse(cls, num: Union[int, float, str]) -> 'Number':
+        try:
+            return cls(float(num))
+        except (ValueError, TypeError) as e:
+            raise ValueError("Invalid numeric value") from e
+
+class Calculator:
+    def process_numbers(self, numbers: Iterable[Union[int, float, str]]) -> Tuple[float, list[float]]:
+        return self._process_numbers(numbers)
+
+    def _process_numbers(self, numbers: Iterable[Union[int, float, str]]) -> Tuple[float, list[float]]:
+        numbers = (Number.parse(n) for n in numbers if isinstance(n, (int, float, str)) and n != '')
+        return sum(n.value for n in numbers), [n.value for n in numbers]
+
+class NumberProcessor(Calculator):
+    pass
+
+def main() -> None:
+    numbers = [1, 2, 'a', 4.5, 6, 7, 8]
+    processor = NumberProcessor()
+    result, data = processor.process_numbers(numbers)
+    print(result)
+    print(data)
